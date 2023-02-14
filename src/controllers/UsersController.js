2142bf000532a2440a84511724341355da976f18
Alexandre Marra <alexsmarra@hotmail.com>
2023-02-14 17:06:29 -0300

    making createNotes migration

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,34 @@
+/*  "hash" is the function that will generate the encryption (criptografia)
+"compare" is to compare the encrypted password with the old_password entered (digitado) by the user   */
+const { hash, compare } = require("bcryptjs")
+
 const AppError = require("../utils/AppError")
 
 class UsersController {
-   create(req, res) {
-      const { name, email, password } = req.bodyefa
+   async create(req, res) {
+      const { name, email, password } = req.body
 
-      if(!name) {
-         throw new AppError("Name is mandatory!")
+      // to database connect
+      const database = await sqliteConnection()
+      // To check if the registered email exists (para verificar se já existe o email cadastrado, para que não tenha dois emails iguais)
+      // Inside the parenthesis the question mark (o ponto de interrogação) is for the variable we want to look for, which in this case is the email, and outside between square brackets (colchetes) we put the variable we want to replace. How many 'question marks (?)' we have, according to their order (de acordo com sua ordem), will be for other variables that we want.
+      const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+      if(checkUserExists) {
+         throw new AppError("This email is already in use.")
       }
+      
+      // For encryption (const hash above). The "8" is for encryption complexity level.
+      const hashedPassword = await hash(password, 8)
 
-      res.status(201).json({ name, email, password })
+      await database.run(
+         "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+         // using the encrypted password (hashedPassword)
+         [name, email, hashedPassword]
+      )
+
+      // we will return an empty json
+      return res.status(201).json()
    }
 }
 
