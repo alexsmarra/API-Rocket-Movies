ac67b590c452d6639a87e9acaeddd552d28a23c4
Alexandre Marra <alexsmarra@hotmail.com>
2023-02-14 10:18:45 -0300

    fixing package.json

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-// 1
 const express = require("express")
 
-// 2
+// vai pegar o index.js automaticamente
+const routes = require("./routes")
+
+
 const app = express()
+app.use(express.json())
 
-// 3
+app.use(routes)
+
 const PORT = 3333
-
-// 4
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
