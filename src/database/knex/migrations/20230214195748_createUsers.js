b56669e16e13cbc6ef6736af44bd9dc001c1003d
Alexandre Marra <alexsmarra@hotmail.com>
2023-02-15 11:15:14 -0300

    fixing the code, because firts it is supposed to be the createUsers migration, there is no 'notes' migration

---
+++ src/database/knex/migrations/20230214195748_createUsers.js
@@ -0,0 +1,20 @@
+exports.up = async function(knex) {
+   const exists = await knex.schema.hasTable("users")
+   if(!exists) {
+      await knex.schema.createTable("users", table => {
+         table.increments("id")
+         table.text("name")
+         table.text("email")
+         table.text("password")
+         table.text("avatar")
+         table.timestamp("created_at").default(knex.fn.now())
+         table.timestamp("updated_at").default(knex.fn.now())
+      })
+   }
+} 
+
+exports.down = knex => knex.schema.dropTable("users")
+
+
+
+
