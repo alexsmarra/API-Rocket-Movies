30d345cf33c0268dd26fd33f29149f18ddbb61f3
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-16 11:43:31 -0300

    fazendo arquivos para uploads de imagens... parte 3, criacao do UserAvatarController.js

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,2 +1,37 @@
+/* Let's take the image that the user sent (which is in the TMP folder) and send it to the folder
+upload.js, and put the image address in our database */
+
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
+const DiskStorage = require("../providers/DiskStorage")
+
+class UserAvatarController {
+   async update(req, res) {
+      const user_id = req.user.id
+      const avatarFileName = req.file.filename
+
+      const diskStorage = new DiskStorage()
+
+      const user = await knex("users")
+         .where({ 'id': user_id }).first()
+
+      if(!user) {
+         throw new AppError("Only authenticated users can change the avatar!", 401)
+      }
+
+      /* If there is an address in user.avatar... whenever the user is going to update the profile
+      picture, we will delete the one that was stored */
+      if(user.avatar) {
+         await diskStorage.deleteFile(user.avatar)
+      }
+      
+      /* Saving the new avatar.. */
+      const filename = await diskStorage.saveFile(avatarFileName)
+      user.avatar = filename
+
+      await knex("users").update(user).where({ id: user_id })
+      return res.json(user)
+   }  
+}
+
+module.exports = UserAvatarController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require("multer")
 const uploadConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController")
+const UserAvatarController = require("../controllers/UserAvatarController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router()
@@ -11,6 +12,7 @@ we would create another variable. */
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController()
+const userAvatarController = new UserAvatarController()
 
 usersRoutes.post("/", usersController.create)
 /* n precisaremos mais de pegar o id pelo params, pois estamos pegando o id do usuário em nosso
@@ -19,9 +21,6 @@ usersRoutes.put("/", ensureAuthenticated, usersController.update)
 usersRoutes.delete("/:id", usersController.delete)
 /* Remembering, the path is to update a specific field, specific column, "upload.single()" is 
 because we want upload only one file, and "avatar" is the column that will be changed */
-usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
-   console.log(req.file.filename)
-   res.json()
-})
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
